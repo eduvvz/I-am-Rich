858dbdd64d7eae577a42765a24b8c66f3764ab36
Eduardo Cerqueira <eduardo.cerqueira@inovacaoeinstein.com.br>
2020-03-17 16:57:59 -0300

    added image of diamond

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,7 @@ void main() => runApp(
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       )),
