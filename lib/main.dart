e0f23f8a2e8df8ba935e28af255c13217427297b
Eduardo Cerqueira <eduardo.cerqueira@inovacaoeinstein.com.br>
2020-03-17 15:34:35 -0300

    added random image and centered

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-  MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('I am rich'),
-      ),
-    ),
-  ),
-);
+      MaterialApp(
+          home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.deepPurpleAccent,
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'),
+          ),
+        ),
+      )),
+    );
